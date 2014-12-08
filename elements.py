064b16e67a3da14e6591e217e3d3eed7e1a85efb
sarochT <n.ato_o@hotmail.com>
2014-12-08 23:48:20 +0700

    Create elements of my game
    player can walk and jump
    running monster  # but need to improve walking more
    Create moving up floor

---
+++ elements.py
@@ -0,0 +1,145 @@
+import pygame
+from pygame.locals import *
+import random
+class Square(object):
+
+    THICKNESS = 10
+    HEIGHT = 630
+    WIDTH = 480
+
+    def __init__(self, pos_y, color):
+        self.pos_y = pos_y
+        self.color = color
+        self.crop = Rect((0, self.HEIGHT-self.pos_y, self.WIDTH, self.THICKNESS))
+
+    def auto_move(self):
+        self.pos_y  -= 1
+        self.crop = Rect((0, self.HEIGHT-self.pos_y, self.WIDTH, self.THICKNESS))
+        if self.HEIGHT - self.pos_y > self.HEIGHT :
+            self.pos_y = self.HEIGHT
+
+    def render(self, surface):
+        pygame.draw.rect(surface, self.color, pygame.Rect(0, self.HEIGHT-self.pos_y, self.WIDTH, self.THICKNESS),2)
+
+#########################################
+
+class Player(object):
+
+    THICKNESS = 1
+    HEIGHT = 630
+    WIDTH = 480
+    POWERTOJUMP = 210
+    ERROR_SIZE = 5
+    VX = 5
+
+    def __init__(self, radius, pos, color, vy, check_jump):
+        (self.x, self.y) = pos
+        self.radius = radius
+        self.color = color
+        self.vy = vy
+        self.check_jump = check_jump
+        self.crop = Rect((self.x, self.HEIGHT - self.y - self.ERROR_SIZE,self.radius*2,self.radius*2))
+
+    def auto_move(self):
+        self.crop = Rect((self.x, self.HEIGHT - self.y - self.ERROR_SIZE,self.radius*2,self.radius*2))
+        if self.check_jump == 1 :
+            self.y += 1 + self.vy/30
+            self.vy -= 3
+            if self.vy <= 0 :
+                self.check_jump = 0
+        elif self.check_jump == 0 :
+            self.y -= (1 + self.vy/20)
+            self.vy += 3
+        elif self.check_jump == 2 :
+            self.y -= 1
+
+    def check_x(self):
+        if self.x < 0 :
+            self.x = self.WIDTH-10
+        elif self.x > self.WIDTH :
+            self.x = 10
+        
+
+    def move_left(self):
+        self.x -= self.VX
+
+    def move_right(self):
+        self.x += self.VX
+
+    def move_jump(self):
+        self.check_jump = 1
+        self.vy = self.POWERTOJUMP
+
+    def render(self, surface):
+        pos = (int(self.x), self.HEIGHT - int(self.y))
+        pygame.draw.circle(surface, self.color, pos, self.radius, 0)
+
+#########################################
+
+class Monster(object):
+
+    THICKNESS = 1
+    HEIGHT = 630
+    WIDTH = 480
+    RADIUS = 15
+    VX  = 1
+    VY = 1
+    BLACK = pygame.Color('black')
+    WHITE = pygame.Color('white')
+    GREEN = pygame.Color('green')
+    YELLOW = pygame.Color('yellow')
+    RED = pygame.Color('red')
+    BLUE = pygame.Color('blue')
+
+    def __init__(self, radius, pos, color, vy , waytowalk):
+        (self.x, self.y) = pos
+        self.radius = radius
+        self.waytowalk = waytowalk
+        self.color = color
+        self.vy = vy
+        self.crop = Rect((self.x, self.HEIGHT - self.y, self.RADIUS*2, self.RADIUS*2))
+
+    def auto_move(self):
+        if self.waytowalk == 1:
+            self.x -= self.VX
+            self.y -= self.VY
+            if self.x <= 0:
+                self.waytowalk = 0
+        elif self.waytowalk == 0:
+            self.x += self.VX
+            self.y -= self.VY
+            if self.x >= self.WIDTH:
+                self.waytowalk = 1
+        if self.HEIGHT-self.y > self.HEIGHT :
+            self.y = self.HEIGHT
+            self.x = random.randint(0,self.WIDTH)
+        self.crop = Rect((self.x, self.HEIGHT - self.y, self.RADIUS*2, self.RADIUS*2))
+
+    def render(self, surface):
+        pos = (int(self.x), self.HEIGHT - int(self.y))
+        pygame.draw.circle(surface, self.color, pos, self.radius, 0)
+
+############################################################
+
+class Coin(object):
+
+    THICKNESS = 1
+    HEIGHT = 630
+    WIDTH = 480
+    RADIUS = 5
+    VY = 1
+    YELLOW = pygame.Color('yellow')
+
+
+    def __init__(self, pos):
+        (self.x, self.y) = pos
+        self.crop = Rect((self.x, self.HEIGHT - self.y, self.RADIUS*2, self.RADIUS*2))
+
+    def auto_move(self):
+        self.y -= 1
+        self.crop = Rect((self.x, self.HEIGHT - self.y, self.RADIUS*2, self.RADIUS*2))
+
+    def render(self, surface):
+        if self.HEIGHT - self.y > 0:
+            pos = (int(self.x), self.HEIGHT - int(self.y))
+            pygame.draw.circle(surface, self.YELLOW, pos, self.RADIUS, 0)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import random
 import gamelib
+from elements import Square,Player,Monster
 
 class Escaping(gamelib.SimpleGame):
     BLACK = pygame.Color('black')
@@ -18,16 +19,130 @@ class Escaping(gamelib.SimpleGame):
 
     def init(self):
         super(Escaping, self).init()
-
+        self.render_score()
     
     def __init__(self):
         super(Escaping, self).__init__('Escaping', Escaping.BLACK)
+        self.player = Player(radius=self.radius, color=Escaping.WHITE, pos=(self.window_size[0]/2, self.window_size[1]/2), vy = 120, check_jump = 0)
+        self.squares = []
+        self.monsters1 = []
+        self.monsters2 = []
+        self.monsters3 = []
+        self.score = 0
+        self.count_time = 0
+        self.time = 0
+        self.init_monster()
+        self.IsGameStarted = True
+        self.init_Square()
+        
+    def init_Square(self):
+        for i in range(0, self.AmountOfSquare) :
+            self.squares.append(Square(pos_y=self.HEIGHT/self.AmountOfSquare*i, color=Escaping.GREEN))
+
+    def init_monster(self):
+        for j in range(0, self.AmountOfMonster) :
+            self.monsters1.append(Monster(radius=self.radius, color=Escaping.BLUE, 
+                pos=(random.randint(0,self.WIDTH), self.HEIGHT/self.AmountOfSquare+15), vy=10, waytowalk=random.randint(0,1)))
+        for j in range(0, self.AmountOfMonster) :
+            self.monsters2.append(Monster(radius=self.radius, color=Escaping.GREEN, 
+                pos=(random.randint(0,self.WIDTH), self.HEIGHT/self.AmountOfSquare*2+15), vy=10, waytowalk=random.randint(0,1)))
+        for j in range(0, self.AmountOfMonster) :
+            self.monsters3.append(Monster(radius=self.radius, color=Escaping.RED, 
+                pos=(random.randint(0,self.WIDTH), self.HEIGHT/self.AmountOfSquare*3+15), vy=10, waytowalk=random.randint(0,1)))
 
     def update(self):
-        pass
+        if self.IsGameStarted :
+            self.auto_move_Square()
+            self.player.auto_move()
+            self.update_CanPlayerJump()
+            self.player.check_x()
+            self.key_pressed()
+            self.update_time()
+            self.update_monster()
+            self.CheckCollision_Between_Monster_Player()
+
+    def update_monster(self):
+        for y in self.monsters1 :
+            y.auto_move()
+        for y in self.monsters2 :
+            y.auto_move()
+        for y in self.monsters3 :
+            y.auto_move()
+
+    def auto_move_Square(self):
+        for x in self.squares :
+            x.auto_move()
+
+    def CheckCollision_Between_Monster_Player(self):
+        if self.time >= 4:
+            for x in self.monsters1 :
+                if self.player.crop.colliderect(x.crop):
+                    self.IsGameStarted = False
+                    print "collide"
+        if self.time >= 8:
+            for x in self.monsters2 :
+                if self.player.crop.colliderect(x.crop):
+                    self.IsGameStarted = False
+                    print "collide"
+        if self.time >= 1:
+            for x in self.monsters3 :
+                if self.player.crop.colliderect(x.crop):
+                    self.IsGameStarted = False
+                    print "collide"
+
+    def update_time(self):
+        self.time = self.count_time/60
+        self.count_time += 1
+        self.render_time()
+
+    def update_CanPlayerJump(self):
+         for x in self.squares :
+            if self.player.crop.colliderect(x.crop) and self.player.check_jump == 0 :
+                self.player.check_jump = 2
+                self.score += 1
+                self.render_score()
+
+    def key_pressed(self):
+        if self.is_key_pressed(K_LEFT):
+            self.player.move_left()
+        elif self.is_key_pressed(K_RIGHT):
+            self.player.move_right()
+        if self.is_key_pressed(K_SPACE):
+            if self.player.check_jump == 2 :
+                self.player.move_jump()
 
     def render(self, surface):
-        pass
+        self.player.render(surface)
+        surface.blit(self.score_image, (10,10))
+        surface.blit(self.time_image,(10,50))
+        self.render_Monster(surface)
+        self.render_Square(surface)
+
+    def render_coin(self):
+        for y in self.coins :
+            y.render(surface)
+
+
+    def render_score(self):
+        self.score_image = self.font.render("Score = %d" % self.score, 0, Escaping.WHITE)
+
+    def render_time(self):
+        self.time_image = self.font.render("Time : %d" % self.time, 0 , Escaping.WHITE)
+
+    def render_Monster(self, surface):
+        if self.time >= 4:
+            for y in self.monsters1:
+                y.render(surface)
+        if self.time >= 8:
+            for y in self.monsters2:
+                y.render(surface)
+        if self.time >= 1:
+            for y in self.monsters3:
+                y.render(surface)
+    def render_Square(self, surface):
+        for x in self.squares:
+            x.render(surface)
+
 
 def main():
     game = Escaping()
